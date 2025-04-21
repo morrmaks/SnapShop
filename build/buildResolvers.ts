d1ef49954cd77de7a7c9b9c1d41e41b147bac339
Maksim Morozov <morozov.maksm20@yandex.ru>
2025-04-21 14:44:09 +0500

    feat: дополнить файл Readme

--- build/buildResolvers.ts
+++ build/buildResolvers.ts
@@ -4,10 +4,6 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export default  function buildResolvers() {
   return {
-    alias: {
-      // '@components': path.resolve(__dirname, '../src/components/'),
-      // '@utils': path.resolve(__dirname, '../src/utils/'),
-    },
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
   }
 }
